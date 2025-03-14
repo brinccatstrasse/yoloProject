6f0a941e0c689c102c2736b9fc9289ef49f23fd3
matthew brincat <matweb2000mt@gmail.com>
2025-03-14 10:39:10 +0100

    inline comment update

--- yolo-project/src/graphql/countryQuery.ts
+++ yolo-project/src/graphql/countryQuery.ts
@@ -10,7 +10,7 @@ export const GET_COUNTRIES = gql`
   }
 `;
 
-// GraphQl query for filtering. Passing $code as a string, and using $code for the filter input
+// GraphQl query for filtering. Parameter $code as a string, and using $code for the filter input
 export const GET_COUNTRIES_BY_CODE = gql`
   query GetCountriesByCode($code: String!) {
     countries(filter: { code: { eq: $code } }) {
